882b2934637f52396c36f99fa9f8ab65036cefab
LC-Deploy Automation <lC-deploy@kissthebride.fr>
2024-01-28 15:28:49 +0100

    add Group

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,40 +17,10 @@ class HomeController extends AbstractController
     public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
         
-        $groupsHeaders = [];
-        $datasessions = []; 
-        $filters = [];
-
-        $postChild = $request->getPayload()->get('child', 0);
-        $postGroup = $request->getPayload()->get('group', 0);
-
-        if(!empty($postChild)) {
-            $child = $entityManager->getRepository(Child::class)->find($postChild);
-            $filters['Groupname'] = $child->getGroupname();
-            $postGroup = $filters['Groupname']->getId();
-        }
-
-
-        if(!empty($postGroup)) {
-            $filters['Groupname'] = $entityManager->getRepository(Group::class)->find($postGroup);
-        }
-
         $groups = $entityManager->getRepository(Group::class)->findAll();
-        $children = $entityManager->getRepository(Child::class)->findAll();
-        $sessions = $entityManager->getRepository(Session::class)->findBy($filters);
-
-        foreach($sessions as $session) {
-            $groupsHeaders[$session->getGroupname()->getId()] = $session->getGroupname()->__toStringForFront();
-            $datasessions[$session->getDate()->__toString()][$session->getGroupname()->getId()] = $session->getName().' : '.$session->getMonitor()->__toString();
-        }
-
+        
         return $this->render('home/index.html.twig', [
             "groups" => $groups,
-            "children" => $children,
-            "groupsHeaders" => $groupsHeaders,
-            "datasessions" => $datasessions,
-            "postChild" => $postChild,
-            "postGroup" => $postGroup,
         ]);
     }
 }
